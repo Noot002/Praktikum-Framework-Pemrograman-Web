ed1b1a09bc3a85dc7026eb2204430ca8af3f5342
Noot002 <ibnurachman79@gmail.com>
2026-09-20 15:35:26 +0700

    Latihan Praktikum 2 - Membuat rute /about menggunakan closure sederhana

--- pos-toko/routes/web.php
+++ pos-toko/routes/web.php
@@ -6,3 +6,8 @@ use App\Http\Controllers\DashboardController;
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth'])
     ->name('dashboard');
+
+Route::get('/about', function () {
+    return '<h3>Profil Toko POS Barokah Mart</h3>
+    <p>Barokah Mart adalah aplikasi Point of Sale (POS) modern yang dirancang untuk mengelola stok, kategori produk, supplier, dan transaksi penjualan secara efisien, aman, dan barokah.</p>';
+});
